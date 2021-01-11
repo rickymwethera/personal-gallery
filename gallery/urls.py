2aacfba16c40753a8e66dfdbaa31b4bf30981787
rickymwethera <rickymwethera@gmail.com>
2021-01-11 10:31:29 +0300

    added the search function

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
-    url('',views.welcome,name = 'welcome'),
+    url('^$',views.welcome,name = 'welcome'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
