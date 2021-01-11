ab9b871ac7da0b86958e5638160f58501cac6da2
rickymwethera <rickymwethera@gmail.com>
2021-01-11 03:56:32 +0300

    created views

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image, Location, Category
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'index.html')
+    images = Image.get_images()
+    locations = Location.get_locations()
+    return render(request, 'index.html',{"images":images, 'locations': locations})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        category = request.GET.get("image")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for an image"
+        return render(request, 'search.html',{"message":message})
+
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    message = f"{location}"
+    return render(request, 'location.html', {"message":message,'images': images})
