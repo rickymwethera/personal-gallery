d3b91d0996ad905ece04a7131ad55c245ffc8077
rickymwethera <rickymwethera@gmail.com>
2021-01-11 03:30:12 +0300

    connected postgres db

--- personalgallery/settings.py
+++ personalgallery/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'personalgallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photoapp',
+        'USER': 'ricky',
+    'PASSWORD':'mwethera99',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
